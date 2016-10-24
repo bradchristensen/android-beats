87238b481ae51c6b41cc147d1415f34354dfdfd4
Brad Christensen <brad@christensen.co.nz>
2016-10-24 23:15:02 +1300

    Add a sidebar

--- Beats.Xamarin.Android.App/MainActivity.cs
+++ Beats.Xamarin.Android.App/MainActivity.cs
@@ -1,117 +1,102 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.Widget;
+using Android.Views;
 using Android.Widget;
+using Beats.Xamarin.Android.App.Views;
 using Beats.Xamarin.Datastore;
 using Beats.Xamarin.Datastore.Models;
 using Beats.Xamarin.WebApiClient;
 using Beats.Xamarin.WebApiClient.Exceptions;
 using System;
 
+using Fragment = Android.App.Fragment;
+
 namespace Beats.Xamarin.Android.App
 {
     [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        private string _server;
-        private string _username;
-        private string _password;
-        private string _sessionId;
-        private DateTimeOffset _sessionExpires;
-
-        private Repository _repository;
-
-        private CherryMusicClient _cherryMusicClient;
-
-        public MainActivity()
-        {
-            _repository = new Repository();
-            _cherryMusicClient = new CherryMusicClient();
-        }
+        private DrawerLayout _drawerLayout;
+        private ActionBarDrawerToggle _drawerToggle;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.DrawerLayout);
 
-            Button loginButton = FindViewById<Button>(Resource.Id.ButtonLogin);
-            EditText serverField = FindViewById<EditText>(Resource.Id.EditTextServer);
-            EditText usernameField = FindViewById<EditText>(Resource.Id.EditTextUsername);
-            EditText passwordField = FindViewById<EditText>(Resource.Id.EditTextPassword);
-            TextView textViewLoginStatus = FindViewById<TextView>(Resource.Id.TextViewLoginStatus);
+            var fragment = new LoginFragment();
 
-            serverField.Text = _repository.LoginDetails?.ServerUrl ?? "";
-            usernameField.Text = _repository.LoginDetails?.Username ?? "";
-            passwordField.Text = _repository.LoginDetails?.Password ?? "";
-            _sessionId = _repository.LoginDetails?.SessionId;
+            var fragmentManager = FragmentManager;
+            var ft = fragmentManager.BeginTransaction();
+            ft.Replace(Resource.Id.ContentFrame, fragment);
+            ft.Commit();
 
-            _server = serverField.Text;
-            _username = usernameField.Text;
-            _password = passwordField.Text;
+            _drawerLayout = FindViewById<DrawerLayout>(Resource.Id.DrawerLayout);
 
-            serverField.TextChanged += delegate
-            {
-                _server = serverField.Text;
-                EnableLoginButton();
-            };
-            usernameField.TextChanged += delegate
-            {
-                _username = usernameField.Text;
-                EnableLoginButton();
-            };
-            passwordField.TextChanged += delegate
-            {
-                _password = passwordField.Text;
-                EnableLoginButton();
-            };
+            ActionBar.SetDisplayHomeAsUpEnabled(true);
+            ActionBar.SetHomeButtonEnabled(true);
 
-            loginButton.Click += async delegate
-            {
-                loginButton.Text = "Logging in...";
-                loginButton.Enabled = false;
-                textViewLoginStatus.Text = "";
+            _drawerToggle = new MyActionBarDrawerToggle(this, _drawerLayout,
+                Resource.Drawable.ic_menu_white_24dp,
+                Resource.String.DrawerOpen,
+                Resource.String.DrawerClosed
+            );
 
-                try
-                {
-                    var cookie = await _cherryMusicClient.Authenticate(_server, _username, _password);
-
-                    _sessionId = cookie.Value;
-                    _sessionExpires = new DateTimeOffset(cookie.Expires);
-
-                    _repository.UpdateLoginDetails(new LoginDetails
-                    {
-                        Username = _username,
-                        Password = _password,
-                        ServerUrl = _server,
-                        SessionExpires = _sessionExpires,
-                        SessionId = _sessionId,
-                    });
-
-                    loginButton.Text = "Logged in!";
-                    loginButton.Enabled = false;
-                    textViewLoginStatus.Text = _sessionId;
-
-                    var dirListing = await _cherryMusicClient.GetDirectoryListing();
-                    foreach (var dir in dirListing)
-                    {
-                        textViewLoginStatus.Text += dir.Label;
-                    }
-                }
-                catch (LoginFailedException)
-                {
-                    EnableLoginButton();
-                    textViewLoginStatus.Text = "";
-                    textViewLoginStatus.Text = "Failed to login.";
-                }
-            };
+            _drawerLayout.AddDrawerListener(_drawerToggle);
         }
 
-        private void EnableLoginButton()
+        private class MyActionBarDrawerToggle : ActionBarDrawerToggle
         {
-            Button loginButton = FindViewById<Button>(Resource.Id.ButtonLogin);
-            loginButton.Text = GetString(Resource.String.LoginButton);
-            loginButton.Enabled = true;
+            MainActivity owner;
+
+            public MyActionBarDrawerToggle(MainActivity activity, DrawerLayout layout, int imgRes, int openRes, int closeRes)
+                : base(activity, layout, imgRes, openRes, closeRes)
+            {
+                owner = activity;
+            }
+
+            public override void OnDrawerClosed(View drawerView)
+            {
+                owner.ActionBar.Title = owner.Title;
+                owner.InvalidateOptionsMenu();
+            }
+
+            public override void OnDrawerOpened(View drawerView)
+            {
+                owner.ActionBar.Title = owner.Title;
+                owner.InvalidateOptionsMenu();
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            // The action bar home/up action should open or close the drawer.
+            // ActionBarDrawerToggle will take care of this.
+            if (_drawerToggle.OnOptionsItemSelected(item))
+            {
+                return true;
+            }
+
+            return true;
+        }
+
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+            // Sync the toggle state after onRestoreInstanceState has occurred.
+            _drawerToggle.SyncState();
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            // Pass any configuration change to the drawer toggls
+            _drawerToggle.OnConfigurationChanged(newConfig);
         }
     }
 }
---
+++ Beats.Xamarin.Android.App/Views/LoginFragment.cs
@@ -0,0 +1,117 @@
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Beats.Xamarin.Datastore;
+using Beats.Xamarin.Datastore.Models;
+using Beats.Xamarin.WebApiClient;
+using Beats.Xamarin.WebApiClient.Exceptions;
+using System;
+
+namespace Beats.Xamarin.Android.App.Views
+{
+    public class LoginFragment : Fragment
+    {
+        private string _server;
+        private string _username;
+        private string _password;
+        private string _sessionId;
+        private DateTimeOffset _sessionExpires;
+
+        private Repository _repository;
+        private CherryMusicClient _cherryMusicClient;
+
+        private View _rootView;
+
+        public LoginFragment()
+        {
+            _repository = new Repository();
+            _cherryMusicClient = new CherryMusicClient();
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle)
+        {
+            _rootView = inflater.Inflate(Resource.Layout.LoginFragment, container, false);
+
+            Button loginButton = _rootView.FindViewById<Button>(Resource.Id.ButtonLogin);
+            EditText serverField = _rootView.FindViewById<EditText>(Resource.Id.EditTextServer);
+            EditText usernameField = _rootView.FindViewById<EditText>(Resource.Id.EditTextUsername);
+            EditText passwordField = _rootView.FindViewById<EditText>(Resource.Id.EditTextPassword);
+            TextView textViewLoginStatus = _rootView.FindViewById<TextView>(Resource.Id.TextViewLoginStatus);
+
+            serverField.Text = _repository.LoginDetails?.ServerUrl ?? "";
+            usernameField.Text = _repository.LoginDetails?.Username ?? "";
+            passwordField.Text = _repository.LoginDetails?.Password ?? "";
+            _sessionId = _repository.LoginDetails?.SessionId;
+
+            _server = serverField.Text;
+            _username = usernameField.Text;
+            _password = passwordField.Text;
+
+            serverField.TextChanged += delegate
+            {
+                _server = serverField.Text;
+                EnableLoginButton();
+            };
+            usernameField.TextChanged += delegate
+            {
+                _username = usernameField.Text;
+                EnableLoginButton();
+            };
+            passwordField.TextChanged += delegate
+            {
+                _password = passwordField.Text;
+                EnableLoginButton();
+            };
+
+            loginButton.Click += async delegate
+            {
+                loginButton.Text = "Logging in...";
+                loginButton.Enabled = false;
+                textViewLoginStatus.Text = "";
+
+                try
+                {
+                    var cookie = await _cherryMusicClient.Authenticate(_server, _username, _password);
+
+                    _sessionId = cookie.Value;
+                    _sessionExpires = new DateTimeOffset(cookie.Expires);
+
+                    _repository.UpdateLoginDetails(new LoginDetails
+                    {
+                        Username = _username,
+                        Password = _password,
+                        ServerUrl = _server,
+                        SessionExpires = _sessionExpires,
+                        SessionId = _sessionId,
+                    });
+
+                    loginButton.Text = "Logged in!";
+                    loginButton.Enabled = false;
+                    textViewLoginStatus.Text = _sessionId;
+
+                    var dirListing = await _cherryMusicClient.GetDirectoryListing();
+                    foreach (var dir in dirListing)
+                    {
+                        textViewLoginStatus.Text += dir.Label;
+                    }
+                }
+                catch (LoginFailedException)
+                {
+                    EnableLoginButton();
+                    textViewLoginStatus.Text = "";
+                    textViewLoginStatus.Text = "Failed to login.";
+                }
+            };
+
+            return _rootView;
+        }
+
+        private void EnableLoginButton()
+        {
+            Button loginButton = _rootView.FindViewById<Button>(Resource.Id.ButtonLogin);
+            loginButton.Text = GetString(Resource.String.LoginButton);
+            loginButton.Enabled = true;
+        }
+    }
+}
